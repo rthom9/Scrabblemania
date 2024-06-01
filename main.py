6bad4803c362f993bf24c570b66a01ec8c8c999e
Rob T <87737757+rthom9@users.noreply.github.com>
2024-06-01 02:04:06 -0600

    Timer and alert are working

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 import client
-import json
 import requests
 import threading
 import time
 import tkinter
 from tkinter import messagebox
-from userUI import set_timer, start_timer, pause_timer, cancel_timer
-
+from userUI import set_timer, start_timer
 
 
 letter_frequency_dict = {
-    "A": 9, "B": 2,} 
-    
-#     "C": 2, "D": 4, "E": 12, "F": 2, "G": 3, "H": 2, "I": 9, "J": 1, 
-#     "K": 1, "L": 4, "M": 2, "N": 6, "O": 8, "P": 2, "Q": 1, "R": 6, "S": 4, "T": 6, 
-#     "U": 4, "V": 2, "W": 2, "X": 1, "Y": 2, "Z": 1,
-# }
+    "A": 9, "B": 2, "C": 2, "D": 4, "E": 12, "F": 2, "G": 3, "H": 2, "I": 9, "J": 1,
+    "K": 1, "L": 4, "M": 2, "N": 6, "O": 8, "P": 2, "Q": 1, "R": 6, "S": 4, "T": 6,
+    "U": 4, "V": 2, "W": 2, "X": 1, "Y": 2, "Z": 1,
+}
 
 letter_points = {
     "A": 1, "B": 3, "C": 3, "D": 2, "E": 1, "F": 4, "G": 2, "H": 4, "I": 1, "J": 8, 
@@ -32,14 +28,15 @@ commands = {
     "commands": "See this list again."
 }
 
-word_entry_time = "000010"
-word_entry_reminder_1 = "000005"
-word_entry_reminder_2 = "000002"
+game_time_limit = "000100"
 
 
-def game_timer(player, t1, t2, t3):
+def game_timer(player):
     root = tkinter.Tk()
     root.withdraw()
+    t1 = "000003"
+    t2 = "000005"
+    t3 = "000000"
     set_timer(t1, t2, t3)
     start_timer()
     while True:
@@ -48,17 +45,18 @@ def game_timer(player, t1, t2, t3):
             if alert == t2:
                 messagebox.showinfo("Alert", f"You have {t2} seconds remaining.")
                 time.sleep(1.1)
-                print("This happened")
             if alert == t3:
                 messagebox.showinfo("Alert", f"You have {t3} seconds remaining.")
                 alerts_file.write("")
                 time.sleep(1)
-                print("This happened")
             if alert == "0s":
                 break
-
-    messagebox.showinfo("Alert", "You are out of time. Game has ended.")
+        #time.sleep(1)
+    print("\nTime is up!")
+    root.wm_attributes("-topmost", 1)
+    messagebox.showinfo(title="Alert", message="You are out of time. Game has ended.", parent=root)
     finished(player)
+
 
 
 class Game:
@@ -66,7 +64,14 @@ class Game:
         self.letter_frequency_dict = letter_frequency_dict
         self.letter_points = letter_points
         self.letter_bag = []
-    
+        self.game_time = None
+        self.reminder_time_1 = "000100"
+        self.reminder_time_2 = None
+
+    def start_game(self, player):
+        self.make_letter_bag()
+        player.new_letters(self.letter_bag)
+
     def make_letter_bag(self):
         letter_bag = []
         for letter in self.letter_frequency_dict:
@@ -84,8 +89,6 @@ class Player:
         self.submitted_words = []
     
     def new_letters(self, letter_bag, number_of_letters=7):
-        #game_timer_thread = threading.Thread(target=game_timer)
-        #game_timer_thread.start()
         if len(letter_bag) < 7:
             number_of_letters = len(letter_bag)
         self.letters = client.randomize_request(letter_bag, number_of_letters)
@@ -129,9 +132,10 @@ def introduction():
     print("Welcome to ScrabbleMania!")
     print("Use this application to practice your word finding skills!")
     print("")
-
-    print("You have been given a set of 7 letters from the letter bag. Enter your best word. Once the bag is near empty, you will be given only the remaining letters.")
-    print("")
+    print("You will be given a set of 7 letters from the letter bag. Enter your best word. \nOnce the bag is near "
+          "empty, you will be given only the remaining letters.")
+    print("Enter the commands listed below to navigate this game.")
+    command_display(commands)
 
 def command_display(commands):
     print("")
@@ -179,24 +183,28 @@ def command_controller(command_type, game, player):
                 print(f"Your letters: {player.letters}")
 
 
+def game_loop():
+    while True:
+        command_type = input("Enter command: ")
+        command_controller(command_type, g1, p1)
 
+
+#Start Game
+introduction()
+#game_time_limit = input("Set a time limit for your game. Please use HHMMSS format. You will be notified when 60s remaining: ")
 
 g1 = Game(letter_frequency_dict, letter_points)
-g1.make_letter_bag()
 p1 = Player()
-introduction()
-command_display(commands)
-p1.new_letters(g1.letter_bag)
-game_timer_thread = threading.Thread(target=game_timer(p1, word_entry_time, word_entry_reminder_1, word_entry_reminder_2))
+
+input("Set a time limit for your game. Please use HHMMSS format. You will be notified when 60s remaining: ")
+game_timer_thread = threading.Thread(target=game_timer, args=([p1]))
+game_loop_thread = threading.Thread(target=game_loop)
+
+g1.start_game(p1)
 game_timer_thread.start()
+game_loop()
+#game_loop_thread.start()
 
-while True:
-    command_type = input("Command: ")
-    command_controller(command_type, g1, p1)
 
-# send_email("thomrobert9@gmail.com", "Yoyoboy!", "It's your time shawty")
-# set_timer("000006","000005","000002")
-# start_timer()
-# print(user_current_letters)
 
 
--- timer.py
+++ timer.py
@@ -26,30 +26,25 @@ def timer_countdown(countdown_time, reminder_1, reminder_2):
             print(countdown_time)
             time.sleep(1)
             countdown_time -= 1
-            if countdown_time == reminder_1 and reminder_2 != 0:
-                with open('alerts.txt', 'r+', encoding='utf-8') as alerts_file:
-                    alerts_file.seek(0)
-                    alerts_file.truncate()
+            with open('alerts.txt', 'r+', encoding='utf-8') as alerts_file:
+                alerts_file.seek(0)
+                alerts_file.truncate()
+                if countdown_time == reminder_1 and reminder_2 != 0:
                     alerts_file.write(time_reverter(reminder_1))
-            if countdown_time == reminder_2 and reminder_1 != 0:
-                with open('alerts.txt', 'r+', encoding='utf-8') as alerts_file:
-                    alerts_file.seek(0)
-                    alerts_file.truncate()
+                if countdown_time == reminder_2 and reminder_1 != 0:
                     alerts_file.write(time_reverter(reminder_2))
-            if countdown_time == 0:
-                with open('alerts.txt', 'r+', encoding='utf-8') as alerts_file:
-                    alerts_file.seek(0)
-                    alerts_file.truncate()
-                    alerts_file.write(str(0) + "s")
-                # change controller.txt to stop
-                with open('controller.txt', 'r+', encoding='utf-8') as controller_file:
-                    controller_file.seek(0)
-                    controller_file.truncate()
-                    controller_file.write("stop")
-                with open('time.txt', 'r+', encoding='utf-8') as time_file:
-                    time_file.seek(0)
-                    time_file.truncate()
-                    time_file.write("000000000000000000")
+                if countdown_time == 0:
+                    with open('alerts.txt', 'r+', encoding='utf-8') as alerts_file:
+                        alerts_file.write(str(0) + "s")
+                    # change controller.txt to stop
+                    with open('controller.txt', 'r+', encoding='utf-8') as controller_file:
+                        controller_file.seek(0)
+                        controller_file.truncate()
+                        controller_file.write("stop")
+                    with open('time.txt', 'r+', encoding='utf-8') as time_file:
+                        time_file.seek(0)
+                        time_file.truncate()
+                        time_file.write("000000000000000000")
         if command == "reset":
             break
     return
